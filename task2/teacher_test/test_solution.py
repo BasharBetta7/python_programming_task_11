7299e15ba852c99b323f3b1668368a3a4f3500b1
BasharBetta7 <{new_email}>
2025-12-10 21:47:57 +0300

    updated test_solution.py:

--- task2/teacher_test/test_solution.py
+++ task2/teacher_test/test_solution.py
@@ -1,23 +1,26 @@
 import subprocess
 import sys
 import inspect
+import pytest
 import re
 
 
-from task2.faulty import mysterious_function
+from ..faulty import mysterious_function
 
 def test_correct_function_behavior():
-    assert mysterious_function([1,2,3,4,5], 3) == 1.8
+    return mysterious_function([1,2,3,4,5], 3) == 1.8
     
 def test_variable_names_correct():
     sig = inspect.signature(mysterious_function)
     params = list(sig.parameters.values())
     pattern = r"[a-z_][a-z0-9_]{2,30}$" 
-    assert len(params) == 0
     for p in params:
-        assert re.fullmatch(pattern, p.name) is not None
+        if re.fullmatch(pattern, p.name) is None:
+            return False
+    return True
 
-
+        
+     
 
 def _run_subprocess(cmd):
     return subprocess.run(
@@ -34,8 +37,7 @@ def test_pylint_clean():
           "pylint",
             "task2/faulty.py"])
   
-
-    assert result.returncode == 0
+    return result.returncode == 0
 
 
 def test_mypy_clean():
@@ -46,6 +48,35 @@ def test_mypy_clean():
           "--strict",
             "task2/faulty.py"])
    
+    return result.returncode == 0
 
-    assert result.returncode == 0
 
+if __name__ == '__main__':
+    score = 0
+    if test_correct_function_behavior() :
+        print("test_correct_function_behavior: PASSED")
+        score += 1
+    else:
+        print(f"test_correct_function_behavior: FAILED")
+
+    if test_variable_names_correct():
+        print(f"test_variable_names_correct: PASSED")
+        score += 1
+    else:
+        print(f"test_variable_names_correct: FAILED")
+    
+    if test_pylint_clean():
+        print(f"test_pylint_clean: PASSED")
+        score += 1
+    else:
+        print(f"test_pylint_clean: FAILED")
+        print(f"run `pylint ./task2/faulty.py` to find errors in your code.")
+    
+    if test_mypy_clean():
+        print(f"test_mypy_clean: PASSED")
+        score += 1
+    else:
+        print(f"test_mypy_clean: FAILED")
+        print(f"run `mypy --strict ./task2/faulty.py` to find and correct your function")
+    
+    print(f"total score: {score}/4")
