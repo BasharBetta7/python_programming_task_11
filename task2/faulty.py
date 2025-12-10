60ac4a5a9ce6300aeb70e4608bd4e267f7296ce4
BasharBetta7 <{new_email}>
2025-12-10 21:11:25 +0300

    added task2 files

---
+++ task2/faulty.py
@@ -0,0 +1,11 @@
+import sys
+
+def mysterious_function(x, y:int) -> int:
+    a = 0
+    for i in x:
+        if i > y :
+            a += i
+    return ans / len(x)
+    return 1
+
+
---
+++ task2/teacher_test/test_solution.py
@@ -0,0 +1,44 @@
+import subprocess
+import pathlib
+import sys
+
+from task2.faulty import mysterious_function
+
+
+
+ROOT = pathlib.Path(__file__).resolve().parents[1]
+
+
+def test_correct_function_behavior():
+    assert mysterious_function([1,2,3,4,5], 3) == 1.8
+
+def _run_subprocess(cmd):
+    return subprocess.run(
+        cmd, 
+        text=True,
+        capture_output=True
+    )
+
+
+def test_pylint_clean():
+    result =  _run_subprocess(
+        [sys.executable,
+          "-m", 
+          "pylint",
+            "task2/faulty.py"])
+  
+
+    assert result.returncode == 0
+
+
+def test_mypy_clean():
+    result =  _run_subprocess(
+        [sys.executable,
+          "-m", 
+          "mypy",
+          "--strict",
+            "task2/faulty.py"])
+   
+
+    assert result.returncode == 0
+
