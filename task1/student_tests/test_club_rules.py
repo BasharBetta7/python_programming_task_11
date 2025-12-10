1f1a350e261bfecd8702949343e9c18346f5ead4
BasharBetta7 <{new_email}>
2025-12-10 15:20:46 +0300

    altered test_club_rules

--- task1/student_tests/test_club_rules.py
+++ task1/student_tests/test_club_rules.py
@@ -1,13 +1,20 @@
 from task1.club_rules import can_join_club
 import pytest
 #--------------------------
-# Equivalence classes tests
+# Equivalence classes tests:
 #--------------------------
 
+
+# example
 def test_inside_range_accepted():
-    pass
+    assert can_join_club(25, 200) is True
+
+# continue writing your test functions, don't forget to start the function name with the word test_
+
+
+
 
 #--------------------------
-# Boundary value tests
+# Boundary value tests:
 #--------------------------
 
