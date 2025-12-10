d230b4e0ab2bf0efd50a2b8f001da31a0cca5914
BasharBetta7 <{new_email}>
2025-12-10 21:23:53 +0300

    updated solution script

--- task2/teacher_test/test_solution.py
+++ task2/teacher_test/test_solution.py
@@ -1,10 +1,23 @@
 import subprocess
 import sys
+import inspect
+import re
+
 
 from task2.faulty import mysterious_function
 
 def test_correct_function_behavior():
     assert mysterious_function([1,2,3,4,5], 3) == 1.8
+    
+def test_variable_names_correct():
+    sig = inspect.signature(mysterious_function)
+    params = list(sig.parameters.values())
+    pattern = r"[a-z_][a-z0-9_]{2,30}$" 
+    assert len(params) == 0
+    for p in params:
+        assert re.fullmatch(pattern, p.name) is not None
+
+
 
 def _run_subprocess(cmd):
     return subprocess.run(
