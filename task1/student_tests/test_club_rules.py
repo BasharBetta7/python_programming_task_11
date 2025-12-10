a3cf1547f1e50de65b1f8571de49a3cb988e31fb
BasharBetta7 <{new_email}>
2025-12-10 15:07:52 +0300

    added contents to student script

---
+++ task1/student_tests/test_club_rules.py
@@ -0,0 +1,13 @@
+from task1.club_rules import can_join_club
+import pytest
+#--------------------------
+# Equivalence classes tests
+#--------------------------
+
+def test_inside_range_accepted():
+    pass
+
+#--------------------------
+# Boundary value tests
+#--------------------------
+
